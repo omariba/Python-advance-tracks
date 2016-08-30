52cc9cd9566e1437e2dba1ec095f92eb6b7b481e
mrlarso <larson.mick@gmail.com>
2016-08-30 14:55:43 +0300

    Create bank.py

---
+++ bank.py
@@ -0,0 +1,22 @@
+""
+You have just started working at a bank, and you discover that they aren't using any computers to keep track of their
+customers!
+
+Create some Python classes that will keep track of your customers.
+Each customer should have a first and second name, as well as an ID number (4 digits). They also have the option to deposit
+money when they open the account (default amount to start is 0 for most customers)
+
+All customers can get their balance for free
+
+There should also be methods in your classes for depositing and withdrawing. See the rules for the different customers below.
+
+Your bank has 3 types of customers:
+Regular -  no minimum balance, starting balance is whatever they put in at the beginning. Charged 50 kes withdrawal fee.
+
+Preferred - Must maintain a minimum balance of 10000 kes, not charged a withdrawl fee.
+
+Student - No minimum balance, no withdrawal fee, but can only withdraw 500 kes at a time.
+
+If a customer tries to withdraw more than is in his/her account, goes below the minimum balance,
+ or if a student tries to withdraw more than 500kes, print out an error message.
+ """
