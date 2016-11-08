4ed7805662ee0caa018d117d506dc8c9cf2808a3
Nelson Omariba <omariba5@gmail.com>
2016-11-08 07:08:06 +0300

    Completed multiples

--- koan_labs/multiples.py
+++ koan_labs/multiples.py
@@ -1,4 +1,10 @@
 def multiple_sum():
-	#TODO: Your code goes here
-	return
-	
+	li = []
+	for i in range(1000):
+		if i%3 == 0 or i%5 == 0:
+			li.append(i)
+			summ = 0
+			for n in li:
+				summ += n
+	return summ
+multiple_sum()
