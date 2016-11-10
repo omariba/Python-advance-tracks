3e81f8e23f32e6b6fe96957c83610023bcaa42ce
Nelson Omariba <omariba5@gmail.com>
2016-11-10 16:12:34 +0300

    Finished positionOfFirstVowel

--- koan_labs/more_string_practice.py
+++ koan_labs/more_string_practice.py
@@ -1,15 +1,21 @@
+vowel = ['A','E','I','O','U','Y','a','e','i','o','u','y']
 def is_Vowel(character):
-	vowel = ['A','E','I','O','U','Y','a','e','i','o','u','y']
 	if character in vowel:
 		return True
 	else:
 		return False
 def position_of_first_vowel(word):
-	#code goes here.
-	return
+	for i in word:
+		if i in vowel:
+			if i == "Y" or i == "y":
+				if word.index(i) == 0:
+					continue
+			else:
+				return word.index(i)
+	return -1
 
 def pigify(word):
 	#code goes here.
 	return
 
-is_Vowel('a')
+position_of_first_vowel('yesterday')
