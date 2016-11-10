0b7bf0454989e58b550df6e8ac248888357e926f
Nelson Omariba <omariba5@gmail.com>
2016-11-10 06:27:17 +0300

    Finished expressions and string count

--- koan_labs/expressions_and_statements.py
+++ koan_labs/expressions_and_statements.py
@@ -1,3 +1,14 @@
 def compounded_principal(time):
-	# Your code here
-	return
+	p = 10000
+	r = 0.08
+	t = time
+	n = 1
+
+	incr_p = r/n
+	bracks = 1 + incr_p
+	up = n*t
+	inn = bracks ** up
+	A = p * inn
+	return int(A)
+
+compounded_principal(30)
