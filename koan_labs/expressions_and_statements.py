8586c1a125d4be3bffe6910f049b283b1b1f9ad8
Nelson Omariba <omariba5@gmail.com>
2016-11-16 08:47:50 +0300

    Completed activity 1

--- koan_labs/expressions_and_statements.py
+++ koan_labs/expressions_and_statements.py
@@ -6,5 +6,8 @@ def compounded_principal(time):
 	inn = bracks ** up
 	A = 10000 * inn
 	return int(A)
+	#A=P(1+r/n)^nt
+	#t = year(s)
+	#n = no of times its compounded per year
 
 compounded_principal(30)
