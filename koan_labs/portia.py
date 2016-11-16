7c6afe67dba543847c760a878180e1cb7ed8eabb
Nelson Omariba <omariba5@gmail.com>
2016-11-16 08:47:50 +0300

    Completed activity 1

--- koan_labs/portia.py
+++ koan_labs/portia.py
@@ -1,8 +1,18 @@
+import math
 
 def calculate_wealth(yearnow):
-    # Your code here
-    return 
+	incr_p = 0.05/1
+	bracks = 1 + incr_p
+	up = yearnow*1
+	inn = bracks ** up
+	A = 1 * inn
+	#A=P(1+r/n)^nt
+	#t = year(s)
+	#n = no of times its compounded per year
+	return math.floor(A)
 
 def find_year():
     # Your code here
     return 
+
+calculate_wealth(100)
