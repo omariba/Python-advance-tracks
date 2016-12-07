68b702b56147bfa56f0366858498c5311cb87795
Nelson Omariba <omariba5@gmail.com>
2016-12-07 10:57:11 +0300

    Completed 21 activities

--- koan_labs/fibonnacci.py
+++ koan_labs/fibonnacci.py
@@ -1,2 +1,12 @@
-def fibonnacci(args):
-	return 
+def fibonacci(args):
+    a=1
+    b=1
+    fibo = [0,1,1]
+    while args >= len(fibo):
+        c = a + b
+        a=b
+        b=c
+        fibo.append(c)
+    return tuple(fibo)
+
+fibonacci(7)
