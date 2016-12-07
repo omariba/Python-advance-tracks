cd0540c5dd694f8689b9eebcf0767d022df09798
Nelson Omariba <omariba5@gmail.com>
2016-12-07 09:30:10 +0300

    Clean up

--- koan_labs/password_integrity.py
+++ koan_labs/password_integrity.py
@@ -23,34 +23,4 @@ def password_integrity(args):
 		else:
 			return True
 	
-print password_integrity("a F1'*'")
-
-# 				if i not in string.uppercase:
-# 					if i not in string.punctuation:
-# 						if i not in str(range(10)):
-# 							return False
-# validator.add(i)
-# continue
-# import string
-# def punc(arg):
-# 	if arg in string.punctuation:
-# 		return True
-# 	else:
-# 		return False
-		# for i in args:
-		# 	if i in string.lowercase:
-		# 		for s in validator:
-		# 			if s.islower() == False:
-		# 				validator.add(i)
-		# 	elif i in string.uppercase:
-		# 		for s in validator:
-		# 			if s.isupper() == False:
-		# 				validator.add(i)
-		# 	elif i in string.punctuation:
-		# 		for s in validator:
-		# 			if punc(s) == False:
-		# 				validator.add(i)
-		# 	elif i in str(range(10)):
-		# 		for s in validator:
-		# 			if s.isdigit() == False:
-		# 				validator.add(i)
+print password_integrity("ABd1234@1")
